08c8a3dd84f22bdb562bcfa1e9d5645a5b435395
Safayet <hossainsafayet187@gmail.com>
2025-07-02 17:59:54 +0600

    Fix: product id from query issue fixed

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,7 +36,7 @@ class ProductController extends Controller
     function ProductByID(Request $request)
     {
         $user_id = $request->header('id');
-        $product_id = $request->query('id');
+        $product_id = $request->input('id');
 
         return Product::where('id', $product_id)->where('user_id', $user_id)->first();
     }
