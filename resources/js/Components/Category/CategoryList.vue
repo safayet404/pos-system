3b1a8468ff60bc9795d0f0b9d41a2b2ae113b257
Safayet <hossainsafayet187@gmail.com>
2025-07-24 19:31:41 +0600

    Fix:Speling

--- resources/js/Components/Category/CategoryList.vue
+++ resources/js/Components/Category/CategoryList.vue
@@ -10,7 +10,7 @@
                                 <Link
                                     class="start-btn btn btn-dark"
                                     href="/category-save"
-                                    >Category Create</Link
+                                    >Categories Create</Link
                                 >
                             </div>
 
